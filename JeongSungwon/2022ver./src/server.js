36eb5f49df6ca40f17ed875519290d2fc521f544
sungwon2054 <chsw93@gmail.com>
2022-11-05 12:40:22 +0900

    (2022ver) 3.5 Middlewares part One

--- JeongSungwon/2022ver./src/server.js
+++ JeongSungwon/2022ver./src/server.js
@@ -4,17 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+};
 const handleHome = (req, res) => {
-  return res.send("I still you love")
-}
+    return res.send("I love middlewares");
+};
+app.get("/", gossipMiddleware, handleHome);
 
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-}
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-
-const handleListening = () => console.log(`Server listning on port http://localhost:${PORT}`);
+const handleListening = () =>
+    console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
