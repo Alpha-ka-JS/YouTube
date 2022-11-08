3a17e48b06cdd7e88e4f4e9ce3d4c3eab31941ed
sungwon2054 <chsw93@gmail.com>
2022-11-08 09:11:32 +0900

    (2022ver) 4.6 Planning Routes

--- JeongSungwon/2022ver./src/routers/userRouter.js
+++ JeongSungwon/2022ver./src/routers/userRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
-import { edit, remove } from "../controllers/userController"
+import { edit, remove, logout, see } from "../controllers/userController"
 
 const userRouter = express.Router();
 
-const handleEdit = (req, res) => res.send("Edit user");
-const handleDelete = (req, res) => res.send("Delete user");
-
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/delete", remove);
+userRouter.get("/remove", remove);
+userRouter.get(":id", see);
 
 export default userRouter;
