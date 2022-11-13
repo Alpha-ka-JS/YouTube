eefbd11a279c97ee5e5987445f49bac2998cea80
sungwon2054 <chsw93@gmail.com>
2022-11-13 12:29:43 +0900

    (2022ver) 6.11 Our First Query

--- JeongSungwon/2022ver./src/controllers/videoController.js
+++ JeongSungwon/2022ver./src/controllers/videoController.js
@@ -1,43 +1,17 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
-
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+export const home = (req, res) => {
+  return res.render("home", { pageTitle: "Home" });
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watching ${video.title}`, video});
+  res.render("watch", { pageTitle: `Watching`});
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  res.render("edit", {pageTitle: `Editing ${video.title}`}, video);
+  res.render("edit", {pageTitle: `Editing`});
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
@@ -52,14 +26,5 @@ export const getUpload = (req, res) => {
 
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideos = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideos);
   return res.redirect("/");
 }
