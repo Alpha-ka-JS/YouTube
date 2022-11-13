2d4be6c578663fd56886feb7387ba8235fee958f
sungwon2054 <chsw93@gmail.com>
2022-11-13 15:45:50 +0900

    (2022ver) 6.16 Creating a Video part Two

--- JeongSungwon/2022ver./src/controllers/videoController.js
+++ JeongSungwon/2022ver./src/controllers/videoController.js
@@ -37,18 +37,17 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 }
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  const video = new Video({
+  await Video.create({
     title: title,
     description: description,
-    createdAt: Date.now(),
+    createdAt: "abc",
     hashtags: hashtags.split(",").map((word) => `#${word}`),
     meta: {
       views: 0,
       rating: 0,
     },
   });
-  console.log(video);
   return res.redirect("/");
 }
