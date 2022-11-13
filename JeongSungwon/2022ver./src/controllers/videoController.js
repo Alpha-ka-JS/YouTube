0f4de0d8ec979c55341472e5fe717dea44a1a40a
sungwon2054 <chsw93@gmail.com>
2022-11-13 20:26:57 +0900

    (2022ver) 6.21 Edit Video part Two

--- JeongSungwon/2022ver./src/controllers/videoController.js
+++ JeongSungwon/2022ver./src/controllers/videoController.js
@@ -33,9 +33,19 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", {pageTitle: `Edit: ${video.title}`, video});
 };
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  videos[id - 1].title = title;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+  await video.save();
   return res.redirect(`/videos/${id}`);
 };
 
@@ -49,7 +59,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title: title,
       description: description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: hashtags.split(",").map((word) => (word.startsWith("#") ? word : `#${word}`)),
       meta: {
         views: 0,
         rating: 0,
